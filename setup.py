13e05266321313bff928879b4af38b43a3590502
sk7n4k3d <nathan_skynet@tutanota.com>
2023-01-18 16:56:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/nathan-skynet/OctoLight_orangepi"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-#plugin_requires = ["OPi.GPIO"]
+plugin_requires = []
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
